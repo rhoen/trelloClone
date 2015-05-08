51fa3318c389eff162b8ca0e018e270dc7a6cee4
rhoen <rhoen.pa@gmail.com>
2015-05-08 15:00:28 -0400

    add list view template, add list.js model

--- app/assets/javascripts/views/lists/show.js
+++ app/assets/javascripts/views/lists/show.js
@@ -1,4 +1,6 @@
 TrelloClone.Views.Lists.Show = Backbone.View.extend({
   template: JST['lists/show'],
-  render: function () {}
+  render: function () {
+    this.$el.html(template({list: this.model}));
+  }
 })
