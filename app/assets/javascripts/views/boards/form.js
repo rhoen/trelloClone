84480bfc3979f9f4c031abebc4bdd76d4567083f
rhoen <rhoen.pa@gmail.com>
2015-05-08 11:47:48 -0400

    add create board event listener and function on form

--- app/assets/javascripts/views/boards/form.js
+++ app/assets/javascripts/views/boards/form.js
@@ -1,7 +1,22 @@
 TrelloClone.Views.Boards.Form = Backbone.View.extend({
   template: JST['boards/form'],
+  events: {
+    "submit form" : "createBoard"
+  },
   render: function () {
     this.$el.html(this.template());
     return this;
+  },
+
+  createBoard: function(event) {
+    event.preventDefault();
+    var board = new TrelloClone.Models.Board();
+    debugger
+    var formData = $(event.currentTarget).serializeJSON();
+    board.set(formData);
+    board.save({}, //callback
+
+    ).bind(this);
   }
+
 })
