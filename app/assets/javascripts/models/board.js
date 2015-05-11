7d9c5df8ce3768570ed124cf7d4a7ea12fb49ea1
rhoen <rhoen.pa@gmail.com>
2015-05-11 11:57:28 -0400

    add parse method to list model, add cards collection and card model

--- app/assets/javascripts/models/board.js
+++ app/assets/javascripts/models/board.js
@@ -1,7 +1,7 @@
 TrelloClone.Models.Board = Backbone.Model.extend({
   urlRoot: "api/boards",
   parse: function(response) {
-
+    debugger
     this.lists = new TrelloClone.Collections.Lists();
     this.lists.add(response.lists);
     delete response.lists
--- app/assets/javascripts/models/list.js
+++ app/assets/javascripts/models/list.js
@@ -1,3 +1,10 @@
 TrelloClone.Models.List = Backbone.Model.extend({
-  urlRoot: "api/lists" //no GET route
+  urlRoot: "api/lists", //no GET route
+  parse: function(response) {
+    debugger
+    this.cards = new TrelloClone.Collections.Cards();
+    this.cards.add(response.cards);
+    delete response.cards;
+    return response;
+  }
 })
