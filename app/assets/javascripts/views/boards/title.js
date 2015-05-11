a4d89fbbd2b50293fb6955237710dae5eee65302
rhoen <rhoen.pa@gmail.com>
2015-05-11 16:18:14 -0400

    add simple styling to board index

--- app/assets/javascripts/views/boards/title.js
+++ app/assets/javascripts/views/boards/title.js
@@ -1,6 +1,6 @@
 TrelloClone.Views.Boards.Title = Backbone.View.extend({
   template: JST["boards/title"],
-  className: "board-title-box"
+  className: "board-title-box clearfix",
   render: function () {
     this.$el.html(this.template({board: this.model}));
     return this;
